bb7c726280a60abe1c2d2b6ec985a434b3596a19
YASMIN NYGATE <YASMIN.NYGATE@GMAIL.COM>
2017-05-21 22:02:04 +0300

    2 views created

--- script/script.js
+++ script/script.js
@@ -9,24 +9,28 @@ changeMainView();
 
 function changeMainView() {
   const homeTemplate = `<div class="wrapper">
-<div class="fixed-bottom">
+<div class="panel fixed-bottom">
+<div class="text">
 <h1>Hello World</h1>
 <p>(Please scroll down)</p>
 </div>
-<div class="layer-one">
+</div>
+<div class="panel layer-one">
 <h2>I'm Hungry</h2>
 </div>
-<div class="layer-two">
+<div class="panel layer-two">
 <h2>I'm Hungry for a job</h2>
 </div>
-<div class="layer-three">
+<div class="panel layer-three">
 <h2>I want a place where I can use my skills and improve on them</h2>
 </div>
-<div class="layer-four">
+<div class="panel layer-four">
 <h3>Want to help me or read more about who I am?</h3>
 </div>
 </div>`;
 
+  const tempTemp = `<span><h1>Please stand by as I create my website and enjoy my lovely CV for now</h1></span>
+    <img class="cv-image" src="./Yasmin%20Nygate%20CV.png">`;
 
   const hash = window.location.hash;
   const header = document.querySelector('header');
@@ -40,6 +44,9 @@ function changeMainView() {
     //   <li><a href="#home">Hello World</a></li>
     //   <li><button type="button">Click me!</button></li>
     // </ul>`;
+  }
+  if (hash === '#temp'){
+    main.innerHTML = tempTemp;
   }
 }
 
